4b7dd1584aabe3de41f8a431063d3282d1082227
Konippi <konippi@amazon.co.jp>
2025-04-29 10:59:01 +0900

    feat(ui): add account details in IAM Identity Center to GUI

--- packages/dashboard-app/src/pages/settings/preferences.tsx
+++ packages/dashboard-app/src/pages/settings/preferences.tsx
@@ -1,11 +1,56 @@
 import { UserPrefView } from "@/components/preference/list";
 import { Button } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
 import settings from "@/data/preferences";
 import { useAuth } from "@/hooks/store/useAuth";
 import { Native, User } from "@aws/amazon-q-developer-cli-api-bindings";
+import { State, Profile } from "@aws/amazon-q-developer-cli-api-bindings";
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+
+type Profile = { profileName: string; arn: string };
 
 export default function Page() {
   const auth = useAuth();
+  const [profile, setProfile] = useState<Profile | undefined>(undefined);
+  const [profiles, setProfiles] = useState<Profile[] | undefined>(undefined);
+
+  useEffect(() => {
+    Profile.listAvailableProfiles()
+      .then(async (res) => {
+        setProfiles(
+          res.profiles.map((p) => ({
+            profileName: p.profileName,
+            arn: p.arn,
+          })),
+        );
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    State.get("api.codewhisperer.profile").then((profile) => {
+      if (typeof profile === "object") {
+        setProfile(profile);
+      }
+    });
+  }, []);
+
+  const onProfileChange = (profile: Profile | undefined) => {
+    setProfile(profile);
+    if (profile) {
+      Profile.setProfile(profile.profileName, profile.arn);
+    }
+  };
 
   let authKind;
   switch (auth.authKind) {
@@ -48,6 +93,60 @@ export default function Page() {
                   : "Logged in"
                 : "Not logged in"}
             </p>
+
+            {auth.authed && auth.authKind === "IamIdentityCenter" && (
+              <>
+                <div className="flex flex-col p-4 mt-2 gap-4 rounded-lg bg-zinc-50 dark:bg-zinc-900 border border-zinc-100 dark:border-zinc-700">
+                  <div className="flex flex-col items-start gap-1">
+                    <h4 className="font-medium leading-none">Start URL</h4>
+                    <Link
+                      href={auth.startUrl ?? ""}
+                      className="font-light leading-tight text-sm text-black/50 dark:text-white/50"
+                    >
+                      {auth.startUrl}
+                    </Link>
+                  </div>
+                  <div className="flex flex-col items-start gap-1">
+                    <h4 className="font-medium leading-none">Region</h4>
+                    <p className="font-light leading-tight text-sm text-black/50 dark:text-white/50">
+                      {auth.region}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="py-4 flex flex-col gap-1">
+                  <h3 className="font-medium leading-none">Active Profile</h3>
+                  <p className="font-light leading-tight text-sm">
+                    SSO users with multiple profiles can select them here
+                  </p>
+                  {profiles ? (
+                    <Select
+                      value={profile?.arn}
+                      onValueChange={(profile) => {
+                        onProfileChange(
+                          profiles?.find((p) => p.arn === profile),
+                        );
+                      }}
+                      disabled={!profiles}
+                    >
+                      <SelectTrigger className="w-60">
+                        <SelectValue placeholder="No Profile Selected" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {profiles &&
+                          profiles.map((p) => (
+                            <SelectItem key={p.arn} value={p.arn}>
+                              {p.profileName}
+                            </SelectItem>
+                          ))}
+                      </SelectContent>
+                    </Select>
+                  ) : (
+                    <Skeleton className="w-60 h-10" />
+                  )}
+                </div>
+              </>
+            )}
             <div className="pt-2">
               <Button
                 variant="outline"
