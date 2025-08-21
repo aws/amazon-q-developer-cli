5f3e986a55b5a99fdd12946bf6cc2474b742712a
Abhishek Anne <abhishek.anne@gmail.com>
2025-08-22 01:54:03 +0530

    Created folder structure for e2e test

---
+++ e2etests/tests/all_tests.rs
@@ -0,0 +1,10 @@
+// Main integration test file that includes all subdirectory tests
+mod agent;
+mod ai_prompts;
+mod context;
+mod core_session;
+mod integration;
+mod mcp;
+mod model;
+mod save_load;
+mod session_mgmt;
