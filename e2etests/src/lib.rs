d65314c330f4b40c167560c49065ef0cffc89c45
sayema Anjum <sayemanc@amazon.com>
2025-08-22 17:23:39 +0530

    Reduce read_response timeout from 20s to 4s

--- e2etests/src/lib.rs
+++ e2etests/src/lib.rs
@@ -138,12 +138,12 @@ pub mod q_chat_helper {
                     },
                     Ok(_) => {
                         // No more data, but wait a bit more in case there's more coming
-                        std::thread::sleep(Duration::from_millis(20000));
+                        std::thread::sleep(Duration::from_millis(4000));
                         if total_content.len() > 0 { break; }
                     },
                     Err(_) => break,
                 }
-                std::thread::sleep(Duration::from_millis(20000));
+                std::thread::sleep(Duration::from_millis(4000));
             }
             
             Ok(total_content)
