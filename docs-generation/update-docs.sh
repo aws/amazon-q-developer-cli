806f45da71adf324fed57f12c213d959384d6ed8
kiran-garre <kiranbug@amazon.com>
2025-08-25 14:57:25 -0700

    Test workflow 3; run second time on existing PR/branch

--- docs-generation/update-docs.sh
+++ docs-generation/update-docs.sh
@@ -9,7 +9,7 @@ fi
 PROMPT="Before making any changes, read the 'docs' directory for the project's current
 documentation. Then read 'pr-contents.txt' to see the contents of the current PR.\n\n
 After reading both the directory and the PR file, update the files in the 'docs' directory 
-with new documentation reflecting the proposed changes in the PR. Make new files as appropriate."
+with new, concise documentation reflecting the proposed changes in the PR. Make new files as appropriate."
 
 timeout 10m echo -e $PROMPT | qchat chat --non-interactive --trust-all-tools
 exit $?
