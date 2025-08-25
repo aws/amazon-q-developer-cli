8a83eeadfb9077c78c6083dfaa516a6c8a89e3cd
kiran-garre <kiranbug@amazon.com>
2025-08-25 13:13:43 -0700

    Try gh pr diff instead of manually getting diff

--- docs-generation/read-pr.sh
+++ docs-generation/read-pr.sh
@@ -7,20 +7,9 @@ PR_NUMBER=$1
 echo "====== PR Information ======\n" > $PR_FILE
 gh pr view $PR_NUMBER --json title,body --jq '"Title: " + .title + "\nDescription: " + .body' >> $PR_FILE
 
-# Include updated files
-echo -e "\n====== Updated files ======\n" >> $PR_FILE
-gh pr view $PR_NUMBER --json files --jq ".files[].path" | while read file; do
-    case "$file" in
-        *.lock|*-lock.*|*.min.*|dist/*|build/*|target/*)
-            continue
-                ;;
-    esac
-    if [ -f "$file" ]; then
-        echo "---- $file ----" >> $PR_FILE
-        git diff main -- "$file" >> $PR_FILE
-        echo -e "\n" >> $PR_FILE
-    fi
-done
+# Include PR diffs
+echo -e "\n====== PR Diffs ======\n" >> $PR_FILE
+gh pr diff $PR_NUMBER >> $PR_FILE
 
 
 
