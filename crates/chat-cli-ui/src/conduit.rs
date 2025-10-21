28dfa3f0ba67d63578a7da1af6b44d5b4b30ce30
dingfeli <dingfeli@amazon.com>
2025-10-21 11:15:46 -0700

    changes conduit sender to tokio unbounded

--- crates/chat-cli-ui/src/conduit.rs
+++ crates/chat-cli-ui/src/conduit.rs
@@ -25,7 +25,7 @@ const CONTINUATION_LINE: &str = " ⋮ ";
 #[derive(thiserror::Error, Debug)]
 pub enum ConduitError {
     #[error(transparent)]
-    Send(#[from] Box<std::sync::mpsc::SendError<Event>>),
+    Send(#[from] Box<tokio::sync::mpsc::error::SendError<Event>>),
     #[error(transparent)]
     Utf8(#[from] std::string::FromUtf8Error),
     #[error("No event set")]
@@ -43,7 +43,7 @@ pub struct ViewEnd {
     // TODO: later on we will need replace this byte array with an actual event type from ACP
     pub sender: tokio::sync::mpsc::Sender<Vec<u8>>,
     /// To receive messages from control about state changes
-    pub receiver: std::sync::mpsc::Receiver<Event>,
+    pub receiver: tokio::sync::mpsc::UnboundedReceiver<Event>,
 }
 
 impl ViewEnd {
@@ -51,127 +51,131 @@ impl ViewEnd {
     /// It takes possible messages from the old even loop and queues write to the output provided
     /// This blocks the current thread and consumes the [ViewEnd]
     pub fn into_legacy_mode(
-        self,
+        mut self,
         theme_source: impl ThemeSource,
         mut stderr: std::io::Stderr,
         mut stdout: std::io::Stdout,
     ) -> Result<(), ConduitError> {
-        while let Ok(event) = self.receiver.recv() {
-            match event {
-                Event::LegacyPassThrough(content) => match content {
-                    LegacyPassThroughOutput::Stderr(content) => {
-                        stderr.write_all(&content)?;
-                        stderr.flush()?;
+        tokio::spawn(async move {
+            while let Some(event) = self.receiver.recv().await {
+                match event {
+                    Event::LegacyPassThrough(content) => match content {
+                        LegacyPassThroughOutput::Stderr(content) => {
+                            stderr.write_all(&content)?;
+                            stderr.flush()?;
+                        },
+                        LegacyPassThroughOutput::Stdout(content) => {
+                            stdout.write_all(&content)?;
+                            stdout.flush()?;
+                        },
                     },
-                    LegacyPassThroughOutput::Stdout(content) => {
-                        stdout.write_all(&content)?;
+                    Event::RunStarted(_run_started) => {},
+                    Event::RunFinished(_run_finished) => {},
+                    Event::RunError(_run_error) => {},
+                    Event::StepStarted(_step_started) => {},
+                    Event::StepFinished(_step_finished) => {},
+                    Event::TextMessageStart(_text_message_start) => {
+                        queue!(stdout, theme_source.success_fg(), Print("> "), theme_source.reset(),)?;
+                    },
+                    Event::TextMessageContent(text_message_content) => {
+                        stdout.write_all(&text_message_content.delta)?;
                         stdout.flush()?;
                     },
-                },
-                Event::RunStarted(_run_started) => {},
-                Event::RunFinished(_run_finished) => {},
-                Event::RunError(_run_error) => {},
-                Event::StepStarted(_step_started) => {},
-                Event::StepFinished(_step_finished) => {},
-                Event::TextMessageStart(_text_message_start) => {
-                    queue!(stdout, theme_source.success_fg(), Print("> "), theme_source.reset(),)?;
-                },
-                Event::TextMessageContent(text_message_content) => {
-                    stdout.write_all(&text_message_content.delta)?;
-                    stdout.flush()?;
-                },
-                Event::TextMessageEnd(_text_message_end) => {
-                    queue!(stderr, theme_source.reset(), theme_source.reset_attributes())?;
-                    execute!(stdout, style::Print("\n"))?;
-                },
-                Event::TextMessageChunk(_text_message_chunk) => {},
-                Event::ToolCallStart(tool_call_start) => {
-                    let ToolCallStart {
-                        tool_call_name,
-                        is_trusted,
-                        mcp_server_name,
-                        ..
-                    } = tool_call_start;
-
-                    queue!(
-                        stdout,
-                        theme_source.emphasis_fg(),
-                        Print(format!(
-                            "🛠️  Using tool: {}{}",
+                    Event::TextMessageEnd(_text_message_end) => {
+                        queue!(stderr, theme_source.reset(), theme_source.reset_attributes())?;
+                        execute!(stdout, style::Print("\n"))?;
+                    },
+                    Event::TextMessageChunk(_text_message_chunk) => {},
+                    Event::ToolCallStart(tool_call_start) => {
+                        let ToolCallStart {
                             tool_call_name,
-                            if is_trusted {
-                                " (trusted)".dark_green()
-                            } else {
-                                "".reset()
-                            }
-                        )),
-                        theme_source.reset(),
-                    )?;
+                            is_trusted,
+                            mcp_server_name,
+                            ..
+                        } = tool_call_start;
 
-                    if let Some(server_name) = mcp_server_name {
                         queue!(
                             stdout,
-                            theme_source.reset(),
-                            Print(" from mcp server "),
                             theme_source.emphasis_fg(),
-                            Print(&server_name),
+                            Print(format!(
+                                "🛠️  Using tool: {}{}",
+                                tool_call_name,
+                                if is_trusted {
+                                    " (trusted)".dark_green()
+                                } else {
+                                    "".reset()
+                                }
+                            )),
                             theme_source.reset(),
                         )?;
-                    }
 
-                    execute!(
-                        stdout,
-                        Print("\n"),
-                        Print(CONTINUATION_LINE),
-                        Print("\n"),
-                        Print(TOOL_BULLET)
-                    )?;
-                },
-                Event::ToolCallArgs(tool_call_args) => {
-                    if let serde_json::Value::String(content) = tool_call_args.delta {
-                        execute!(stdout, style::Print(content))?;
-                    } else {
-                        execute!(stdout, style::Print(tool_call_args.delta))?;
-                    }
-                },
-                Event::ToolCallEnd(_tool_call_end) => {
-                    // noop for now
-                },
-                Event::ToolCallResult(_tool_call_result) => {
-                    // noop for now (currently we don't show the tool call results to users)
-                },
-                Event::StateSnapshot(_state_snapshot) => {},
-                Event::StateDelta(_state_delta) => {},
-                Event::MessagesSnapshot(_messages_snapshot) => {},
-                Event::Raw(_raw) => {},
-                Event::Custom(_custom) => {},
-                Event::ActivitySnapshotEvent(_activity_snapshot_event) => {},
-                Event::ActivityDeltaEvent(_activity_delta_event) => {},
-                Event::ReasoningStart(_reasoning_start) => {},
-                Event::ReasoningMessageStart(_reasoning_message_start) => {},
-                Event::ReasoningMessageContent(_reasoning_message_content) => {},
-                Event::ReasoningMessageEnd(_reasoning_message_end) => {},
-                Event::ReasoningMessageChunk(_reasoning_message_chunk) => {},
-                Event::ReasoningEnd(_reasoning_end) => {},
-                Event::MetaEvent(_meta_event) => {},
-                Event::ToolCallRejection(tool_call_rejection) => {
-                    let ToolCallRejection { reason, name, .. } = tool_call_rejection;
+                        if let Some(server_name) = mcp_server_name {
+                            queue!(
+                                stdout,
+                                theme_source.reset(),
+                                Print(" from mcp server "),
+                                theme_source.emphasis_fg(),
+                                Print(&server_name),
+                                theme_source.reset(),
+                            )?;
+                        }
 
-                    execute!(
-                        stderr,
-                        theme_source.error_fg(),
-                        Print("Command "),
-                        theme_source.warning_fg(),
-                        Print(name),
-                        theme_source.error_fg(),
-                        Print(" is rejected because it matches one or more rules on the denied list:"),
-                        Print(reason),
-                        Print("\n"),
-                        theme_source.reset(),
-                    )?;
-                },
+                        execute!(
+                            stdout,
+                            Print("\n"),
+                            Print(CONTINUATION_LINE),
+                            Print("\n"),
+                            Print(TOOL_BULLET)
+                        )?;
+                    },
+                    Event::ToolCallArgs(tool_call_args) => {
+                        if let serde_json::Value::String(content) = tool_call_args.delta {
+                            execute!(stdout, style::Print(content))?;
+                        } else {
+                            execute!(stdout, style::Print(tool_call_args.delta))?;
+                        }
+                    },
+                    Event::ToolCallEnd(_tool_call_end) => {
+                        // noop for now
+                    },
+                    Event::ToolCallResult(_tool_call_result) => {
+                        // noop for now (currently we don't show the tool call results to users)
+                    },
+                    Event::StateSnapshot(_state_snapshot) => {},
+                    Event::StateDelta(_state_delta) => {},
+                    Event::MessagesSnapshot(_messages_snapshot) => {},
+                    Event::Raw(_raw) => {},
+                    Event::Custom(_custom) => {},
+                    Event::ActivitySnapshotEvent(_activity_snapshot_event) => {},
+                    Event::ActivityDeltaEvent(_activity_delta_event) => {},
+                    Event::ReasoningStart(_reasoning_start) => {},
+                    Event::ReasoningMessageStart(_reasoning_message_start) => {},
+                    Event::ReasoningMessageContent(_reasoning_message_content) => {},
+                    Event::ReasoningMessageEnd(_reasoning_message_end) => {},
+                    Event::ReasoningMessageChunk(_reasoning_message_chunk) => {},
+                    Event::ReasoningEnd(_reasoning_end) => {},
+                    Event::MetaEvent(_meta_event) => {},
+                    Event::ToolCallRejection(tool_call_rejection) => {
+                        let ToolCallRejection { reason, name, .. } = tool_call_rejection;
+
+                        execute!(
+                            stderr,
+                            theme_source.error_fg(),
+                            Print("Command "),
+                            theme_source.warning_fg(),
+                            Print(name),
+                            theme_source.error_fg(),
+                            Print(" is rejected because it matches one or more rules on the denied list:"),
+                            Print(reason),
+                            Print("\n"),
+                            theme_source.reset(),
+                        )?;
+                    },
+                }
             }
-        }
+
+            Ok::<(), ConduitError>(())
+        });
 
         Ok(())
     }
@@ -192,7 +196,7 @@ pub type InputReceiver = tokio::sync::mpsc::Receiver<Vec<u8>>;
 pub struct ControlEnd<T> {
     pub current_event: Option<Event>,
     /// Used by the control to send state changes to the view
-    pub sender: std::sync::mpsc::Sender<Event>,
+    pub sender: tokio::sync::mpsc::UnboundedSender<Event>,
     /// Flag indicating whether structured events should be sent through the conduit.
     /// When true, the control end will send structured event data in addition to
     /// raw pass-through content, enabling richer communication between layers.
@@ -381,7 +385,7 @@ pub fn get_legacy_conduits(
     ControlEnd<DestinationStderr>,
     ControlEnd<DestinationStdout>,
 ) {
-    let (state_tx, state_rx) = std::sync::mpsc::channel::<Event>();
+    let (state_tx, state_rx) = tokio::sync::mpsc::unbounded_channel::<Event>();
     let (byte_tx, byte_rx) = tokio::sync::mpsc::channel::<Vec<u8>>(10);
 
     (
