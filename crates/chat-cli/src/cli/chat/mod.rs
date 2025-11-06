f15d2877fcbe45abc92446388e915fe5b41bc7b3
evanliu048 <evannliu@amazon.com>
2025-11-06 13:21:59 -0800

    feat: Skip changelog display on first chat usage (#3378)
    * add a value in statue db
    
    * Revert "add a value in statue db"
    
    This reverts commit 5c3ef7bc24377e82d0ed7a1a6bed9b5f38b11933.
    
    * delete extra data value
    
    * only need to set show count

--- crates/chat-cli/src/cli/chat/mod.rs
+++ crates/chat-cli/src/cli/chat/mod.rs
@@ -1193,7 +1193,12 @@ impl ChatSession {
         // Check if version changed or if we haven't shown it max times yet
         let should_show = match &last_version {
             Some(last) if last == current_version => show_count < CHANGELOG_MAX_SHOW_COUNT,
-            _ => true, // New version or no previous version
+            Some(_) => true, // New version only
+            None => {
+                // Don't show on first time but record the version
+                os.database.set_changelog_last_version(current_version)?;
+                false
+            },
         };
 
         if should_show {
